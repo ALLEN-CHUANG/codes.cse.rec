b8ad3a7d611d2b31c32378e4e274d68a5639375a
chihming <changecandy@gmail.com>
2018-07-30 11:59:36 +0800

    temp block

--- example/train_youtube.sh
+++ example/train_youtube.sh
@@ -1,6 +1,8 @@
 set -x
 Threads=1
 
+echo 'this part is block for now'
+
 # download youtube-links dataset
 zipfile="youtube-links.txt.gz"
 if test -e "$zipfile";
@@ -14,8 +16,8 @@ fi
 zcat youtube-links.txt.gz | awk -F '	' '{print $1" "$2" 1"}' > net.txt
 
 # run the comment
-../cli/deepwalk -train net.txt -save rep_dw.txt -undirected 1 -dimensions 64 -walk_times 1 -walk_steps 40 -window_size 5 -negative_samples 5 -alpha 0.025 -threads $Threads
-../cli/walklets -train net.txt -save rep_wl.txt -undirected 1 -dimensions 64 -walk_times 1 -walk_steps 40 -window_min 2 -window_max 5 -negative_samples 5 -alpha 0.025 -threads $Threads
-../cli/line -train net.txt -save rep_line1.txt -undirected 1 -order 1 -dimensions 64 -sample_times 10 -negative_samples 5 -alpha 0.025 -threads $Threads
-../cli/line -train net.txt -save rep_line2.txt -undirected 1 -order 2 -dimensions 64 -sample_times 10 -negative_samples 5 -alpha 0.025 -threads $Threads
-../cli/hpe -train net.txt -save rep_hpe.txt -undirected 1 -dimensions 64 -sample_times 10 -walk_steps 5 -negative_samples 5 -alpha 0.025 -threads $Threads
+#../cli/deepwalk -train net.txt -save rep_dw.txt -undirected 1 -dimensions 64 -walk_times 1 -walk_steps 40 -window_size 5 -negative_samples 5 -alpha 0.025 -threads $Threads
+#../cli/walklets -train net.txt -save rep_wl.txt -undirected 1 -dimensions 64 -walk_times 1 -walk_steps 40 -window_min 2 -window_max 5 -negative_samples 5 -alpha 0.025 -threads $Threads
+#../cli/line -train net.txt -save rep_line1.txt -undirected 1 -order 1 -dimensions 64 -sample_times 10 -negative_samples 5 -alpha 0.025 -threads $Threads
+#../cli/line -train net.txt -save rep_line2.txt -undirected 1 -order 2 -dimensions 64 -sample_times 10 -negative_samples 5 -alpha 0.025 -threads $Threads
+#../cli/hpe -train net.txt -save rep_hpe.txt -undirected 1 -dimensions 64 -sample_times 10 -walk_steps 5 -negative_samples 5 -alpha 0.025 -threads $Threads
